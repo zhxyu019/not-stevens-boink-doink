acc3f9b51074d37024fdbc186ca5172369794e97
Zhiyu <zhiyuma2006@gmail.com>
2022-07-23 09:18:39 +0800

    Initial Commit

---
+++ boink/boinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  boinkApp.swift
+//  boink
+//
+//  Created by TinkerTanker on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct boinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
