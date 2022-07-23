f0fe3c80e456c70f53731a75ea81b559c4438fd1
Zhiyu <zhiyuma2006@gmail.com>
2022-07-23 10:01:33 +0800

    first

--- boink/ContentView.swift
+++ boink/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = [""]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(emojis, id: \.self) { emoji in
+                Text(emoji)
+                
+            }
+            .navigationTitle("Emojis")
+            .navigation
+        }
     }
 }
 
